51c2d660239ae462c0a5ce5131b87b2af6bb1c8a
Jmeas <jellyes2@gmail.com>
2018-05-26 14:41:58 -0700

    Update Standard Resource

--- src/contexts/resources.js
+++ src/contexts/resources.js
@@ -2,22 +2,13 @@ import createResourceContext from "../react-resources";
 
 const actions = {
   createBook: store => (id, attributes) => {
-    store.update("resources.books", {
-      [id]: {
-        id,
-        meta: {
-          loading: true
-        }
-      }
-    });
+    store.update(`resources.books.${id}.meta.loading`, false);
 
     setTimeout(() => {
-      store.update("resources.books", {
-        [id]: {
-          attributes,
-          meta: {
-            loading: false
-          }
+      store.update(`resources.books.${id}`, {
+        attributes,
+        meta: {
+          loading: false
         }
       });
     }, 2000);
